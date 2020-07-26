732ba5fa211fed1c886298d4bc3dc386d6ae7343
iseol <iseol421@gmail.com>
2020-07-26 17:37:01 +0900

    20.07.26f2 +PlayerAction

---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    Animator anim;
+    float h;
+    float v;
+    bool isHorizonMove;
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move Value
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        // Check Button Up & Down
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        // Check Horizontal Move
+        if (hDown)
+        {
+            isHorizonMove = true;
+        }
+        else if (vDown)
+        {
+            isHorizonMove = false;
+        }
+        else if (hUp || vUp)
+        {
+            isHorizonMove = h != 0; /* 동시에 a, d키를 누르고 한 쪽을 떼었을 때,
+                                      예를 들어 왼쪽을 떼었을 때 h값은 1이 되므로 1은 0이 아니기 때문에 isHorizonMove = true가 된다. */
+        }
+        // Animation
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+        {
+            anim.SetBool("isChange", false);
+        }
+
+        rigid.velocity = new Vector2(rigid.velocity.x, 0);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * 4;
+    }
+}
